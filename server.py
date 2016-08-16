a812d83dc8c8dc5a944797c21576e893d973da0c
pedfx <pedroxavier1@gmail.com>
2016-08-16 18:04:07 -0400

    1

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 import os
 import urllib
 from flask import Flask, request
@@ -10,18 +9,16 @@ import zipfile
 import shutil
 from test import Classificar
 
-
-
 UPLOAD_FOLDER = str(os.getcwd())
-ALLOWED_EXTENSIONS = set(['jpeg','zip'])
+ALLOWED_EXTENSIONS = set(['jpeg', 'zip'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 def allowed_file(filename):
 	return '.' in filename and \
 		   filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -38,16 +35,15 @@ def upload_file():
 		x = 0
 		content1 = ''
 		web = ''
-		if ra!= "" and dec!= "" :
-
-			x =1
+		if ra != "" and dec != "":
+			x = 1
 			ra = float(ra)
 			dec = float(dec)
-			web = 'http://skyserver.sdss.org/dr12/SkyserverWS/ImgCutout/getjpeg?'\
-					'TaskName=Skyserver.Chart.Image&ra='\
-					+ str(ra) + '&dec=' + str(dec) + \
-					'&width=512&height=512&scale=0.2'
-			name = "RA{"+str(ra)+"}DEC{"+str(dec)+"}.jpeg"
+			web = 'http://skyserver.sdss.org/dr12/SkyserverWS/ImgCutout/getjpeg?' \
+				  'TaskName=Skyserver.Chart.Image&ra=' \
+				  + str(ra) + '&dec=' + str(dec) + \
+				  '&width=512&height=512&scale=0.2'
+			name = "RA{" + str(ra) + "}DEC{" + str(dec) + "}.jpeg"
 			path = (os.path.join(app.config['UPLOAD_FOLDER'], name))
 			urllib.urlretrieve(web, path)
 			size = 80, 80
@@ -57,22 +53,19 @@ def upload_file():
 			shutil.move(path, UPLOAD_FOLDER + '/static/galaxies/' +
 						path.replace(str(os.getcwd()), "").replace("/", ""))
 
-
 			web = "<a href= 'http://skyserver.sdss.org/dr12/en/tools/chart/image.aspx?ra=" \
 				  + str(ra) + "&dec=" + str(dec) + \
 				  "&width=512&height=512&scale=0.2'> " \
 				  "Open on SDSS website.</a><br>"
-
 
 			content1 = path
 
 			class1 = Classificar()
 			content2 = class1.rodar(x)
 			class2 = HtmlReturn()
-			html = class2.htmlstring(content1, content2, x, web )
+			html = class2.htmlstring(content1, content2, x, web)
 
 			return html
-
 
 		if 'file' not in request.files:
 			return app.send_static_file('index.html')
@@ -81,7 +74,6 @@ def upload_file():
 
 		if file.filename == '':
 			return app.send_static_file('index.html')
-
 
 		if file and allowed_file(file.filename):
 
@@ -92,7 +84,7 @@ def upload_file():
 			print extension
 
 			if 'jpeg' in extension:
-				x =1
+				x = 1
 				filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 				img = Image.open(filepath)
 				size = 80, 80
@@ -103,32 +95,28 @@ def upload_file():
 							filepath.replace(str(os.getcwd()), "").replace("/", ""))
 				content1 = filepath + "<br>"
 
-
 			if 'zip' in extension:
 				zip = zipfile.ZipFile(filename, 'r')
 				zip.extractall()
 				x = 0
 				content1 = ''
 				for index in glob.glob(os.path.join(app.config['UPLOAD_FOLDER'], '*.jpeg')):
-					x+=1
+					x += 1
 					content1 += index + "<br>"
-					shutil.move(index, UPLOAD_FOLDER+'/static/galaxies/'+
+					shutil.move(index, UPLOAD_FOLDER + '/static/galaxies/' +
 								index.replace(str(os.getcwd()), "").replace("/", ""))
 
-		if x!= 0:
-				class1 = Classificar()
-				content2 = class1.rodar(x)
-				class2 = HtmlReturn()
-				html = class2.htmlstring(content1, content2, x, web)
+		if x != 0:
+			class1 = Classificar()
+			content2 = class1.rodar(x)
+			class2 = HtmlReturn()
+			html = class2.htmlstring(content1, content2, x, web)
 		else:
-				html = app.send_static_file('error.html')
+			html = app.send_static_file('error.html')
 
 		return html
 
-
-
 	return app.send_static_file('index.html')
-
 
 
 port = os.getenv('PORT', '5000')
